e1763243f292fd8ea08c54ff337ccedce8680fb8
cedric.trinh.etu <cedric.trinh.etu@univ-lille.fr>
2026-03-13 21:21:33 +0100

    first commit

---
+++ search.py
@@ -0,0 +1,24 @@
+import secrets
+import sys
+
+def main(nom_fichier):
+    # J'ajoute encoding="utf-8" car un dictionnaire français contient des accents !
+    with open(nom_fichier, "r", encoding="utf-8") as file :
+        tab = file.readlines()
+
+    n = secrets.randbelow(len(tab))
+    return tab[n].strip()
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        fichier_choisi = sys.argv[1]
+        try:
+            s = ""
+            for i in range(5):
+                s += main(fichier_choisi) + "-"
+            s = s[:-1]
+            print(s)
+        except FileNotFoundError:
+            print(f"Erreur : Le fichier '{fichier_choisi}' est introuvable.")
+    else:
+        print("Erreur : Il manque le nom du fichier. Exemple : python search.py mon_fichier.txt")
